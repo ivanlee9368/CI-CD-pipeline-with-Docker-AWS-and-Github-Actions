213eda88aa1f7806cc7b116712b9f7c4f574694d
ivanlee9368 <ivanlee9368@gmail.com>
2022-07-23 13:06:06 +0800

    test6

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,4 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-//Live updating tests
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
